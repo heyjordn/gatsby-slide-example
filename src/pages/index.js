0457b993661c16a03233ce4f230f7bd9cc72bb41
Jordan Jones <proxima.aust@gmail.com>
2019-04-17 17:22:37 -0500

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SlideShow from "../components/slideshow"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <SlideShow />
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
