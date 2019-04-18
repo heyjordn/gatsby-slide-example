1492c1c1ca7ab384b990b86ce2169832c2c4ca38
Jordan Jones <proxima.aust@gmail.com>
2019-04-17 22:23:20 -0500

    Added github link

--- src/components/header.js
+++ src/components/header.js
@@ -11,6 +11,8 @@ const Header = ({ siteTitle }) => (
   >
     <div
       style={{
+        display: "grid",
+        gridTemplateColumns: "300px 1fr 100px",
         margin: `0 auto`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
@@ -27,6 +29,15 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
+      <div />
+      <h4>
+        <a
+          style={{ color: "white" }}
+          href="https://github.com/Pr0x1m4/gatsby-slide-example"
+        >
+          github
+        </a>
+      </h4>
     </div>
   </header>
 )
